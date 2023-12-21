3de5464e2881ea2dcfc686609739c724ae54667a
Андрей Антропов <andreyantropov.1994@gmail.com>
2023-12-21 17:05:09 +0500

    timer

--- Project/pomodoro/src/Header/Header.tsx
+++ Project/pomodoro/src/Header/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
           <img src="/img/logo.svg" alt="Логотип Pomodoro" />
           <a className={styles.statistics} href="">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g id="equalizer" clip-path="url(#clip0_23511_232)">
+              <g id="equalizer" clipPath="url(#clip0_23511_232)">
                 <path id="Vector" d="M10 20H14V4H10V20ZM4 20H8V12H4V20ZM16 9V20H20V9H16Z" fill="#DC3E22"/>
               </g>
               <defs>
--- Project/pomodoro/src/Pomodoro/Timer/Timer.tsx
+++ Project/pomodoro/src/Pomodoro/Timer/Timer.tsx
@@ -3,6 +3,23 @@ import styles from './timer.module.css';
 
 export function Timer() {
   return (
-    <></>
+    <div className={styles.timerComponent}>
+      <div className={styles.titleContainer}>
+        <h3 className={styles.title}>Сверстать сайт</h3>
+        <span className={styles.tomato}>Помидор 1</span>
+      </div>
+
+      <span className={styles.timer}>25:00</span>
+
+      <div className={styles.taskContainer}>
+        <span className={styles.number}>Зачада 1 - </span>
+        <span className={styles.task}>Сверстать сайт</span>
+      </div>
+
+      <div className={styles.btnContainer}>
+        <button className={styles.startBtn}>Старт</button>
+        <button className={styles.stopBtn}>Стоп</button>
+      </div>
+    </div>
   );
 }
