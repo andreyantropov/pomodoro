40c5ba8cfed4336860811f1d71a62c4678ac8387
Андрей Антропов <andreyantropov.1994@gmail.com>
2023-12-22 17:26:33 +0500

    page-not-found

--- Project/pomodoro/src/PageNotFound/PageNotFound.tsx
+++ Project/pomodoro/src/PageNotFound/PageNotFound.tsx
@@ -3,6 +3,12 @@ import styles from './pagenotfound.module.css';
 
 export function PageNotFound() {
   return (
-    <span>Ошибка 404 - страница не найдена</span>
+    <section>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <span className={styles.error}>Ошибка 404 </span><span className={styles.description}> - страница не найдена :(</span>
+        </div>
+      </div>
+    </section>
   );
 }
