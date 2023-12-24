1b344922f0a618473ce36c3bdff40895553223aa
andrey.antropov <andreyantropov.1994@gmail.com>
2023-12-24 13:57:22 +0500

    dropdown

--- Project/pomodoro/src/Pomodoro/TaskList/Task/Task.tsx
+++ Project/pomodoro/src/Pomodoro/TaskList/Task/Task.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './task.module.css';
+import { DropDown } from '../../../DropDown';
 
 export function Task() {
   return (
@@ -8,13 +9,26 @@ export function Task() {
         <span className={styles.number}>1</span>
         <p className={styles.task}>Сверстать сайт</p>
       </div>
-      <button className={styles.menu}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="6" viewBox="0 0 26 6" fill="none">
-          <circle cx="3" cy="3" r="3" fill="#C4C4C4"/>
-          <circle cx="13" cy="3" r="3" fill="#C4C4C4"/>
-          <circle cx="23" cy="3" r="3" fill="#C4C4C4"/>
-        </svg>
-      </button>
+      <div className={styles.menu}>
+        <DropDown
+          button={
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="6" viewBox="0 0 26 6" fill="none">
+                <circle cx="3" cy="3" r="3" fill="#C4C4C4"/>
+                <circle cx="13" cy="3" r="3" fill="#C4C4C4"/>
+                <circle cx="23" cy="3" r="3" fill="#C4C4C4"/>
+              </svg>
+            </button>
+          }
+        >
+          <ul className={styles.dropdown}>
+            <li>Увеличить</li>
+            <li>Увеличить</li>
+            <li>Увеличить</li>
+            <li>Увеличить</li>
+          </ul>
+        </DropDown>
+      </div>
     </li>
   );
 }
