77846acba59a51a70990ab460b3cca72e06daa94
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-04 14:14:42 +0500

    react-select

--- Project/pomodoro/src/Statistics/Title/Interval/Interval.tsx
+++ Project/pomodoro/src/Statistics/Title/Interval/Interval.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './interval.module.css';
+import Select, { ActionMeta } from 'react-select';
+
+type Option = {
+  value: string;
+  label: string;
+}
+
+type SingleValue<Option> = Option | null;
+
+const options: Option[] = [
+  { value: '0', label: 'Эта неделя' },
+  { value: '1', label: 'Прошлая неделя' },
+  { value: '2', label: '2 недели назад' },
+];
 
 export function Interval() {
+  const [selectedOption, setSelectedOption] = useState<SingleValue<Option> | null>(options[0]);
+
+  const handleChange = (option: SingleValue<Option> | null) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <select name="interval" id="interval">
-      <option value="0">Эта неделя</option>
-      <option value="1">Прошлая неделя</option>
-      <option value="2">2 недели назад</option>
-    </select>
+    <Select
+        value={selectedOption}
+        onChange={option => handleChange(option)}
+        options={options}
+    />
   );
 }
