9e90a21b64d4c798d95929c2ee53235bd6b5f4d6
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-04 13:24:48 +0500

    plot-markup

--- Project/pomodoro/src/Icon/StopIcon/StopIcon.tsx
+++ Project/pomodoro/src/Icon/StopIcon/StopIcon.tsx
@@ -11,8 +11,8 @@ export function StopIcon({ size = 130, className = styles.stop }: StopIconProps)
     <svg width={size} height={size} viewBox="0 0 129 129" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="alarm-clock">
           <g id="Group 1241">
-              <path className={className} id="Vector" d="M64.3158 118.632C94.3136 118.632 118.632 94.3136 118.632 64.3158C118.632 34.318 94.3136 10 64.3158 10C34.318 10 10 34.318 10 64.3158C10 94.3136 34.318 118.632 64.3158 118.632Z" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path className={className} id="Vector_2" d="M28 27L102 101" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path className={className} id="Vector" d="M64.3158 118.632C94.3136 118.632 118.632 94.3136 118.632 64.3158C118.632 34.318 94.3136 10 64.3158 10C34.318 10 10 34.318 10 64.3158C10 94.3136 34.318 118.632 64.3158 118.632Z" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"/>
+              <path className={className} id="Vector_2" d="M28 27L102 101" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"/>
           </g>
       </g>
     </svg>
--- Project/pomodoro/src/Statistics/Plot/Plot.tsx
+++ Project/pomodoro/src/Statistics/Plot/Plot.tsx
@@ -1,9 +1,71 @@
 import React from 'react';
 import styles from './plot.module.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartOptions,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options: ChartOptions<"bar"> = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false
+    }
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false
+      },
+    },
+    y: {
+      position: 'right',
+      min: 0,
+      max: 1000,
+      ticks: {
+        stepSize: 500,
+      },
+      border: {
+        display: false
+      },
+    },
+  },
+};
+
+const labels = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: '',
+      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+      backgroundColor: '#ea8a79',
+    },
+  ],
+};
 
 export function Plot() {
   return (
     <div className={styles.plotComponent}>
+      <Bar options={options} data={data} />
     </div>
   );
 }
