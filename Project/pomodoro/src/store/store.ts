428f474d36086c127964d40b9af0e7a9a5c25709
andrey.antropov <andreyantropov.1994@gmail.com>
2024-01-05 14:41:47 +0500

    storeon-localstorage

--- Project/pomodoro/src/store/store.ts
+++ Project/pomodoro/src/store/store.ts
@@ -3,9 +3,10 @@ import { TasksState, TasksEvents, tasksModule } from "./tasks";
 import { TimerState, TimerEvents, timerModule } from "./timer";
 import { StatisticsEvents, StatisticsState, statisticsModule } from "./statistics";
 import { SettingsEvents, SettingsState, settingsModule } from "./settings";
+import { persistState } from "@storeon/localstorage";
 
 export interface State extends TasksState, TimerState, StatisticsState, SettingsState { }
 
 export interface Events extends TasksEvents, TimerEvents, StatisticsEvents, SettingsEvents { }
   
-export const store = createStoreon<State, Events>([tasksModule, timerModule, statisticsModule, settingsModule])
+export const store = createStoreon<State, Events>([tasksModule, timerModule, statisticsModule, settingsModule, persistState(['tasks', 'statistics', 'settings'])]);
