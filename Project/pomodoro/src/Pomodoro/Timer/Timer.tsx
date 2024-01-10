30ecc1cd9c3b075251f3275ef2b3d49db51e9740
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-10 10:55:26 +0500

    timer-task-component

--- Project/pomodoro/src/Pomodoro/Timer/Timer.tsx
+++ Project/pomodoro/src/Pomodoro/Timer/Timer.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import styles from './timer.module.css';
 import { Title } from './Title';
-import { Events, State } from '../../store/store';
-import { useStoreon } from 'storeon/react';
 import { Clock } from './Clock';
 import { Buttons } from './Buttons/Buttons';
-import { useCurrentTask } from '../../hooks/useCurrentTask';
+import { Task } from './Task';
 
 export function Timer() {
-  const { timer, settings } = useStoreon<State, Events>('timer', 'settings');
-  const [ currentTask ] = useCurrentTask();
-
   return (
     <div className={styles.timerComponent}>
       <Title />
       <Clock />
-
-      <div className={styles.taskContainer}>
-        <span className={styles.number}>Задача 1 - </span>
-        <span className={styles.task}>{currentTask ? currentTask.text : 'Задача отсутствует'}</span>
-      </div>
-
+      <Task />
       <Buttons />
     </div>
   );
