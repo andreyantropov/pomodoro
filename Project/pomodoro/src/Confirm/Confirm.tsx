41a3d4b6c5a8d82115a8ea1a79df5df9cd284ad1
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-10 12:13:17 +0500

    cinfirm-dialog-background-fix

--- Project/pomodoro/src/Confirm/Confirm.tsx
+++ Project/pomodoro/src/Confirm/Confirm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import styles from './confirm.module.css';
 import { Icon } from '../Icon';
 import { Icons } from '../enums/Icons';
