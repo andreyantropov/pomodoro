af7217d5b1957543740f0b87994b1d1bb112290c
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-11 13:57:41 +0500

    time-component-weekday-fix

--- Project/pomodoro/src/Statistics/Time/Time.tsx
+++ Project/pomodoro/src/Statistics/Time/Time.tsx
@@ -2,11 +2,43 @@ import React, { useEffect, useState } from 'react';
 import styles from './time.module.css';
 import { useSelectedStat } from '../../hooks/useSelectedStat';
 import { HOUR, MIN, SEC } from '../../constants/time';
+import { WeekDays } from '../../enums/WeekDays';
+import { useStoreon } from 'storeon/react';
+import { State, Events } from '../../store/store';
 
 export function Time() {
   const [ selectedStat ] = useSelectedStat();
+  const { selectedDate } = useStoreon<State, Events>('selectedDate');
+  const [weekDay, setWeekDay] = useState('Воскресенье');
   const [hours, setHours] = useState('0');
   const [minutes, setMinutes] = useState('0');
+
+  useEffect(() => {
+    const weekDayNumber = new Date(selectedDate).getDay();
+    switch (weekDayNumber) {
+      case WeekDays.Sunday:
+        setWeekDay('Воскресенье');
+        break;
+      case WeekDays.Monday:
+        setWeekDay('Понедельник');
+        break;
+      case WeekDays.Tuesday:
+        setWeekDay('Вторник');
+        break;
+      case WeekDays.Wednesday:
+        setWeekDay('Среда');
+        break;
+      case WeekDays.Thursday:
+        setWeekDay('Четверг');
+        break;
+      case WeekDays.Friday:
+        setWeekDay('Пятница');
+        break;
+      case WeekDays.Saturday:
+        setWeekDay('Суббота');
+        break;
+    }
+  }, [selectedDate]);
 
   useEffect(() => {
     if (selectedStat) {
@@ -19,7 +51,7 @@ export function Time() {
   
   return (
     <div className={styles.timeComponent}>
-      <h4 className={styles.title}>Суббота</h4>
+      <h4 className={styles.title}>{weekDay}</h4>
       <span className={styles.description}>
         {!selectedStat && "Нет данных"}
         {selectedStat && (
