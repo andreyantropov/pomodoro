94de3a447b00ff667cc7bfec4dbc2cb1ad1878a1
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-01-12 15:21:48 +0500

    theme-toggle

--- Project/pomodoro/src/Settings/ThemeToggle/ThemeToggle.tsx
+++ Project/pomodoro/src/Settings/ThemeToggle/ThemeToggle.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './themetoggle.module.css';
+import Switch from "react-switch";
 
 interface ThemeToggleProps {
   checked: boolean;
-  OnChange?: () => void;
+  OnChange: () => void;
 }
 
 export function ThemeToggle({ checked, OnChange }: ThemeToggleProps) {
   return (
     <label className={styles.themeToggle}>
-      <input type="checkbox" checked={checked} onChange={OnChange} />
+      <Switch checked={checked} uncheckedIcon={false} checkedIcon={false} offColor='#999' onColor='#a8b64f' activeBoxShadow='0 0 2px 3px #a1ab5f' onChange={() => OnChange()} />
       <span className={styles.label}>Темная тема</span>
     </label>
   );
